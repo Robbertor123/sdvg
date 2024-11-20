3b775d04785064a9ed3429348603af25155a585b
Robbertor123 <153746938+Robbertor123@users.noreply.github.com>
2024-11-20 18:33:43 +0300

    Ним
    //#Бесит Я.Лицей

--- qkn.py
+++ qkn.py
@@ -1,69 +1,95 @@
 import sys
+import random
 
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt6.QtWidgets import QLabel, QLineEdit
+from PyQt6.QtWidgets import QLabel, QLCDNumber
 
 
-class Arifmometr(QWidget):
+class NimStrikesBack(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('Арифмометр')
-        self.setFixedSize(300, 90)
+        self.Y = random.randint(1, 10)
+        self.Z = random.randint(-10, -1)
+        self.k = 10
+        self.X = random.randint(1, 20)
 
-        self.first_value = QLineEdit(self)
-        self.first_value.setText('0')
-        self.first_value.move(10, 10)
-        self.first_value.resize(40, 30)
+        self.setWindowTitle('Ним наносит ответный удар')
+        self.setFixedSize(400, 440)
 
-        self.second_value = QLineEdit(self)
-        self.second_value.setText('0')
-        self.second_value.move(140, 10)
-        self.second_value.resize(40, 30)
+        self.result_label = QLabel(self)
+        self.result_label.setText('          Привет, начинаем игру!                                                  ')
+        self.result_label.move(100, 15)
 
-        self.add_button = QPushButton(self)
-        self.add_button.setText('+')
-        self.add_button.move(50, 10)
-        self.add_button.resize(30, 30)
+        self.btnp = QPushButton(self)
+        self.btnp.setText('+' + str(self.Y))
+        self.btnp.move(40, 70)
+        self.btnp.resize(80, 40)
 
-        self.substract_button = QPushButton(self)
-        self.substract_button.setText('-')
-        self.substract_button.move(80, 10)
-        self.substract_button.resize(30, 30)
+        self.btnm = QPushButton(self)
+        self.btnm.setText(str(self.Z))
+        self.btnm.move(280, 70)
+        self.btnm.resize(80, 40)
 
-        self.multiply_button = QPushButton(self)
-        self.multiply_button.setText('*')
-        self.multiply_button.move(110, 10)
-        self.multiply_button.resize(30, 30)
+        self.q = QLabel(self)
+        self.q.setText('Осталось ходов')
+        self.q.move(40, 190)
 
-        self.result = QLineEdit(self)
-        self.result.setText('0')
-        self.result.setReadOnly(True)
-        self.result.move(190, 10)
-        self.result.resize(40, 30)
+        self.qwa = QLabel(self)
+        self.qwa.setText('Текущее число')
+        self.qwa.move(40, 320)
 
-        self.label = QLabel(self)
-        self.label.setText('=')
-        self.label.move(180, 15)
+        self.result = QLCDNumber(self)
+        self.result.display(self.k)
+        self.result.move(270, 180)
+        self.result.resize(110, 40)
+
+        self.qw = QLCDNumber(self)
+        self.qw.display(self.X)
+        self.qw.move(270, 310)
+        self.qw.resize(110, 40)
+
+        self.ok()
         self.start()
 
+    def ok(self):
+        if self.k <= 0:
+            if self.X == 0:
+                self.result_label.setText("Вы победили, начинаем новую игру")
+            else:
+                self.result_label.setText("Вы проиграли, начинаем новую игру")
+            self.Y = random.randint(1, 10)
+            self.Z = random.randint(-10, -1)
+            self.k = 10
+            self.X = random.randint(1, 20)
+            self.qw.display(self.X)
+            self.result.display(self.k)
+            self.btnp.setText('+' + str(self.Y))
+            self.btnm.setText(str(self.Z))
+
     def start(self):
-        self.add_button.clicked.connect(self.on_click)
-        self.substract_button.clicked.connect(self.wow)
-        self.multiply_button.clicked.connect(self.rex)
+        self.btnp.clicked.connect(self.on_click)
+        self.btnm.clicked.connect(self.wow)
 
     def wow(self):
-        self.result.setText(f"{int(self.first_value.text()) - int(self.second_value.text())}")
-
-    def rex(self):
-        self.result.setText(f"{int(self.first_value.text()) * int(self.second_value.text())}")
+        self.result_label.setText('')
+        self.k -= 1
+        self.result.display(self.k)
+        self.X += self.Z
+        self.qw.display(self.X)
+        self.ok()
 
     def on_click(self):
-        self.result.setText(f"{int(self.first_value.text()) + int(self.second_value.text())}")
+        self.result_label.setText('')
+        self.k -= 1
+        self.result.display(self.k)
+        self.X += self.Y
+        self.qw.display(self.X)
+        self.ok()
 
-#бу, испугался?
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    fc = Arifmometr()
+    fc = NimStrikesBack()
     fc.show()
     sys.exit(app.exec())
